1c688142acb25ba5f8c7759629404c8e792219f3
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-09-26 13:16:17 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./App.scss";
+import Title from "../Title/Title";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="page">
+        <Title />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,9 @@
+import "./Title.scss";
+
+export default function Title() {
+  return (
+    <h1 className="title">
+      Исторические <br /> даты
+    </h1>
+  );
+}
